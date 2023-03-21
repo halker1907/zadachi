8d93c4e6c96c228cd45c88c4150613a6ad900d76
halker1907 <113712157+halker1907@users.noreply.github.com>
2023-03-21 19:44:21 +0300

    Add files via upload

---
+++ hourdhf.py
@@ -0,0 +1,18 @@
+class Player:
+
+	def __init__(self, name="часы", colors="чёрный"):
+		self.name = name
+		self.colors = colors
+
+	def time(self):
+		self.time += 1
+		print(f"{time}")
+
+
+
+p1 = Player()
+print(p1.name)
+print(p1.colors)
+p1.time
+
+
